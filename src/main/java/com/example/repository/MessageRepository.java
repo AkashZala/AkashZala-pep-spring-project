4339bd11f1647feca10e7a00425a5bfab7cdf6f5
Akash Zala <avz9sq@gmail.com>
2024-08-05 19:54:41 +0000

    completed implemention of retrieving messages by account id

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,6 +1,5 @@
 package com.example.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.example.entity.Message;
 import java.util.List;
